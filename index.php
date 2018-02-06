d9d8117edc9d054de93c3d401010af159e6d7610
Ramon Alfonso Moreno de la Madrid <ramon-moreno@users.noreply.github.com>
2018-02-05 18:21:13 -0600

    Start

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Banshee´s Veil</title>
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/libs/base.css">
+
+		<link rel="stylesheet" type="text/css" href="assets/css/views/index.css">
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="assets/js/libs/base.js"></script>
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<nav class="navbar navbar-inverse">
+					<div class="container-fluid ">
+				    <div class="navbar-header col-md-2 col-xs-2">
+				     <img class="logo" src="" alt="" width="100%" height="100%">
+				    </div>
+						<?php //  <<<<<<< HEAD ?>
+
+						<br>
+						<div class="menu">
+					    <ul class="nav navbar-nav col-md-12 col-xs-12 holi">
+					      <li class=""><a href="#"> Home </a></li>
+					      <li class="dropdown">
+					        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1
+					        <span class="caret"></span></a>
+					        <ul class="dropdown-menu">
+					          <li><a href="#">Page 1-1</a></li>
+					          <li><a href="#">Page 1-2</a></li>
+					          <li><a href="#">Page 1-3</a></li>
+										<li><p>-</p> </li>
+										<li><p>-</p> </li>
+										<li><p>-</p> </li>
+										<li><p>-</p> </li>
+					        </ul>
+					      </li>
+					      <li><a href="#">Page 2</a></li>
+					      <li><a href="#">Page 3</a></li>
+							</ul>
+						</div>
+						<?php //=======  ?>
+				  </div>
+				</nav>
+			</div>
+		</header>
+		<main>
+
+		</main>
+		<footer>
+
+		</footer>
+	</body>
+</html>
